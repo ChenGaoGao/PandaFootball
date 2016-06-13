d22c3269bc7c5ec93d5246b7833ce2c0a1cbc909
Oliver Chen <CityOnline_1@oliverdemacbook-pro.local>
2016-06-13 18:10:07 +0800

    0.0

--- PandaFootball/Sections/PDFMyCenter/MyCourse/MyCourseHome/MyCourseScheduleTemplate/MyCourseScheduleTemplateCell.m
+++ PandaFootball/Sections/PDFMyCenter/MyCourse/MyCourseHome/MyCourseScheduleTemplate/MyCourseScheduleTemplateCell.m
@@ -11,6 +11,11 @@
 
 #import "PDFSpaceView.h"
 
+
+const static CGFloat kIconViewHeight            = 15.0f;
+const static CGFloat kButtonWidth               = 36.0f;
+const static CGFloat kButtonHeight              = 49.0f;
+
 @interface MyCourseScheduleTemplateCell ()
 
 @property (nonatomic, strong) PDFSpaceView *spaceView;
@@ -24,6 +29,7 @@
     if (self) {
         [self addSubview:self.spaceView];
         [self addSubview:self.iconView];
+        [self addSubview:self.titleLabel];
         [self addSubview:self.editButton];
         [self addSubview:self.deleteButton];
     }
@@ -56,10 +62,51 @@
     if (_iconView) {
         return _iconView;
     }
-    
     _iconView = [[UIImageView alloc] init];
+    _iconView.frame = CGRectMake(PDFSpaceBigger, VIEW_BOTTOM(_spaceView) + PDFSpaceBiggish, kIconViewHeight, kIconViewHeight);
     
     return _iconView;
+}
+
+- (UILabel *)titleLabel {
+    if (_titleLabel) {
+        return _titleLabel;
+    }
+    _titleLabel = [[UILabel alloc] init];
+    _titleLabel.frame = CGRectMake(VIEW_RIGHT(_iconView) + PDFSpaceSmallest,
+                                   VIEW_BOTTOM(_spaceView) + PDFSpaceBiggish,
+                                   MAIN_WIDTH - (VIEW_RIGHT(_iconView) + PDFSpaceSmallest)
+                                   - (kButtonWidth * 2 + PDFSpaceSmallest),
+                                   kButtonHeight);
+    
+    _titleLabel.font = PDFFontBodySmaller;
+    _titleLabel.textColor = PDFColorTextBodyLight;
+    
+    return _titleLabel;
+}
+
+- (UIButton *)editButton {
+    if (_editButton) {
+        return _editButton;
+    }
+    _editButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _editButton.frame = CGRectMake(VIEW_RIGHT(_titleLabel), VIEW_BOTTOM(_spaceView), kButtonWidth, kButtonHeight);
+    
+    _editButton.backgroundColor = PDFColorRed;
+    
+    return _editButton;
+}
+
+- (UIButton *)deleteButton {
+    if (_deleteButton) {
+        return _deleteButton;
+    }
+    _deleteButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _deleteButton.frame = CGRectMake(VIEW_RIGHT(_editButton), VIEW_BOTTOM(_spaceView), kButtonHeight, kButtonHeight);
+    
+    _deleteButton.backgroundColor = PDFColorGreen;
+    
+    return _deleteButton;
 }
 
 @end
